d332bbcf603d1d0a2199a949c01387f99ebe0e4b
雪文杰 <873813122@qq.com>
2018-05-06 20:42:43 +0800

    Update url.js

--- src/constant/url.js
+++ src/constant/url.js
@@ -1,15 +1,7 @@
-// export const httpURI = "http://47.104.95.55";//上线
+
 export const httpURI = "http://www.luyingjc.com";
 
-export const baseURIShop = httpURI+"/mobile1/";//上线商城部分
-export const baseURI = httpURI+"/application/api/?act=";//德强上线部分
+export const baseURIShop = httpURI+"/mobile1/";//商城部分
+export const baseURI = httpURI+"/application/api/?act=";
 
-// export const httpURIde = "http://192.168.1.112";//德强测试
-// export const baseURI = httpURIde+"/KemaoEC/application/api/?act=";//德强部分
-
-// export const socketURI = "ws://dt.yikezhu.com";
-// export const socketMsgURI = socketURI+":8080/kzwy/websocket/socketServer.jhtml"; //socket消息路径
-
-// http://192.168.1.178/KemaoEC/application/api/?act=  //德强测试地址
-// http://192.168.1.238:8081/mobile1/index_api.php //戴磊测试地址
 
